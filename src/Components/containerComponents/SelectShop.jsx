758830d1b3e8cc029a3e1193a481569894bd294b
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2025-01-12 19:02:39 +0200

    adjust layout and responsive image

--- src/Components/containerComponents/SelectShop.jsx
+++ src/Components/containerComponents/SelectShop.jsx
@@ -1,4 +1,5 @@
-// eslint-disable-next-line react/prop-types
+import PropTypes from "prop-types";
+
 function SelectShop({ selectedShop, setSelectedShop, setInputIsDisabled }) {
   const shops = [
     {
@@ -35,30 +36,38 @@ function SelectShop({ selectedShop, setSelectedShop, setInputIsDisabled }) {
 
   const handleSelectShop = (shop) => {
     setSelectedShop(shop);
-    setInputIsDisabled(false)
+    setInputIsDisabled(false);
   };
 
   return (
     <div className="text-center space-y-2 mb-10">
       <h2 className="title">Select Shop:</h2>
-      <div className=" md:flex space-x-20">
+      <div className="flex flex-col justify-center gap-4">
         {shops.map((shop, index) => (
-          <label key={index}>
-            <div className="flex space-x-5 border-2 border-gray-800 p-2  hover:bg-gray-800 text-zinc-100 rounded-full justify-center">
-              <input
-                type="checkbox"
-                checked={selectedShop.id === shop.id ? shop : !shop}
-                onChange={() => handleSelectShop(shop)}
-                className="mx-2 w-4 accent-gray-800  bg-gray-100 border-gray-300   "
-              />
-              {shop.name}
-              <img className="imgStyle" src={`${shop.logo}`} alt="" />
-            </div>
+          <label
+            key={index}
+            className="flex items-center justify-between space-x-2 border-2 border-gray-800 p-2 hover:bg-gray-800 text-zinc-100 rounded-full"
+          >
+            <input
+              type="checkbox"
+              checked={selectedShop.id === shop.id}
+              onChange={() => handleSelectShop(shop)}
+              className="w-4 h-4 accent-gray-800 bg-gray-100 border-gray-300"
+            />
+            <span>{shop.name}</span>
+            <img className="imgStyle" src={shop.logo} alt={shop.name} />
           </label>
         ))}
       </div>
     </div>
   );
 }
+SelectShop.propTypes = {
+  selectedShop: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+  setSelectedShop: PropTypes.func.isRequired,
+  setInputIsDisabled: PropTypes.func.isRequired,
+};
 
 export default SelectShop;
